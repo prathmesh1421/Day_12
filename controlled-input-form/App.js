3a3986f2a75237d4cdc8fb4b3d35034c9e487a9f
prathmesh joshi <147234424+prathmesh1421@users.noreply.github.com>
2026-04-28 17:34:57 +0530

    Create App.js

---
+++ controlled-input-form/App.js
@@ -0,0 +1,15 @@
+// Import ControlledForm component
+import ControlledForm from "./ControlledForm";
+
+// Main App component
+function App() {
+  return (
+    <div>
+      {/* Render the controlled form component */}
+      <ControlledForm />
+    </div>
+  );
+}
+
+// Export App component
+export default App;
